2c9f0b70615cfcc90eb730ed720274de70503a72
YOzhikS <yozhik-s@ya.ru>
2020-08-30 22:02:24 +0300

    home work 3

--- lesson3/project/js/main.js
+++ lesson3/project/js/main.js
@@ -1,84 +1,225 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать на Promise (не fetch!!!)
-let getRequest = (url, cb) => {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status !== 200) {
-        console.log('Error');
-      } else {
-        cb(xhr.responseText);
+let getRequest = (url) => {
+  return new Promise((resolve,reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject('Error');
+        } else {
+          resolve(xhr.responseText);
+        }
       }
-    }
-  };
-  xhr.send();
+    };
+    xhr.send();
+  });
 };
 
-class ProductList {
-  #goods;
-
-  constructor(container = '.products') {
+class List {
+  constructor(url, container, list = listContext){
     this.container = container;
-    this.#goods = [];
+    this.list = list;
+    this.url = url;
+    this.goods = [];
     this.allProducts = [];
-    // this.#fetchGoods();
-    this.#getProducts()
-        .then(data => {
-          this.#goods = [...data];
-          this.#render();
-        });
+    this.filtered = [];
+    this._init();
   }
 
-  // #fetchGoods() {
-  //   getRequest(`${API}/catalogData.json`, (data) => {
-  //     this.#goods = JSON.parse(data);
-  //     this.#render();
-  //   });
-  // }
-  #getProducts() {
-    return fetch(`${API}/catalogData.json`)
-        .then(result => result.json())
+  getJson(url){
+    return getRequest(url ? url : `${API + this.url}`)
+        .then(result => JSON.parse(result))
         .catch(error => {
           console.log(error);
         });
   }
 
-  #render() {
+  handleData(data){
+    this.goods = [...data];
+    this.render();
+  }
+
+  cakcSum(){
+    return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+  }
+
+  render(){
     const block = document.querySelector(this.container);
-
-    for (let product of this.#goods) {
-      const productObject = new ProductItem(product);
-
+    for (let product of this.goods) {
+      console.log(this.constructor.name);
+      const productObject = new this.list[this.constructor.name](product);
+      console.log(productObject);
       this.allProducts.push(productObject);
       block.insertAdjacentHTML('beforeend', productObject.render());
     }
   }
 
-  getGoodsPrice() {
-    return this.#goods.reduce((sumPrice, { price }) => sumPrice + price, 0);
+  filter(value){
+    const regexp = new RegExp(value, 'i');
+    this.filtered = this.allProducts.filter(product => regexp.test(product.product_name));
+    this.allProducts.forEach(el => {
+      const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+      if(!this.filtered.includes(el)){
+        block.classList.add('invisible');
+      } else {
+        block.classList.remove('invisible');
+      }
+    })
+  }
+
+  _init(){
+    return false
   }
 }
 
-class ProductItem {
-  constructor(product, img='https://placehold.it/200x150') {
-    this.title = product.title;
-    this.price = product.price;
-    this.id = product.id;
+class Item {
+  constructor(el, img = 'https://placehold.it/200x150'){
+    this.product_name = el.product_name;
+    this.price = el.price;
+    this.id_product = el.id_product;
     this.img = img;
   }
 
   render() {
-    return `<div class="product-item" data-id="${this.id}">
+    return ``;
+  }
+}
+
+class ProductList extends List {
+  constructor(cart, container = '.products', url = "/catalogData.json") {
+    super(url, container);
+    this.cart = cart;
+    this.getJson()
+        .then(data => this.handleData(data));
+  }
+
+  _init(){
+    document.querySelector(this.container).addEventListener('click', el => {
+      if (el.target.classList.contains('buy-btn')){
+        this.cart.addProduct(el.target);
+      }
+    })
+  }
+}
+
+class ProductItem extends Item {
+  render() {
+    return `<div class="product-item" data-id="${this.id_product}">
               <img src="${this.img}" alt="Some img">
               <div class="desc">
-                  <h3>${this.title}</h3>
+                  <h3>${this.product_name}</h3>
                   <p>${this.price} \u20bd</p>
-                  <button class="buy-btn">Купить</button>
+                  <button class="buy-btn"
+                  data-id="${this.id_product}"
+                  data-name="${this.product_name}"
+                  data-price="${this.price}">Купить</button>
               </div>
           </div>`;
   }
 }
 
-const list = new ProductList();
+class Cart extends List{
+  constructor(container = ".cart-block", url = "/getBasket.json"){
+    super(url, container);
+    this.getJson()
+      .then(data => {
+        this.handleData(data.contents);
+      });
+  }
+
+  addProduct(element){
+    this.getJson(`${API}/addToBasket.json`)
+      .then(data => {
+        if(data.result === 1){
+          let productId = +element.dataset['id'];
+          let find = this.allProducts.find(product => product.id_product === productId);
+          if(find){
+            find.quantity++;
+            this._updateCart(find);
+            } else {
+              let product = {
+                id_product: productId,
+                price: +element.dataset['price'],
+                product_name: element.dataset['name'],
+                quantity: 1
+              };
+              this.goods = [product];
+              this.render();
+            }
+        } else {
+          alert('Error');
+        }
+      })
+  }
+
+  removeProduct(element){
+    this.getJson(`${API}/deleteFromBasket.json`)
+      .then(data => {
+        if (data.result === 1) {
+          let productId = +element.dataset['id'];
+          let find = this.allProducts.find(product => product.id_product === productId);
+          if (find.quantity > 1){
+            find.quantity--;
+            this._updateCart(find);
+          } else {
+            this.allProducts.splice(this.allProducts.indexOf(find), 1);
+            document.querySelector(`.cart-item[data-id="${productId}"]`).remove();
+          }
+        } else {
+          alert('Error');
+        }
+      })
+  }
+
+  _updateCart(product){
+    let block = document.querySelector(`.cart-item[data-id="${product.id_product}"]`);
+    block.querySelector('.product-quantity').textContent = `Количество: ${product.quantity}`;
+    block.querySelector('.product-price').textContent = `${product.quantity * product.price} ₽`;
+  }
+
+  _init(){
+    document.querySelector('.btn-cart').addEventListener('click', () => {
+      document.querySelector(this.container).classList.toggle('invisible');
+    });
+    document.querySelector(this.container).addEventListener('click', el => {
+      if (el.target.classList.contains('del-btn')){
+        this.removeProduct(el.target);
+      }
+    })
+  }
+}
+
+class CartItem extends Item{
+  constructor(el, img = 'https://placehold.it/50x100'){
+    super(el, img);
+    this.quantity = el.quantity;
+  }
+  render(){
+    return `<div class="cart-item" data-id="${this.id_product}">
+            <div class="product-bio">
+            <img src="${this.img}" alt="Some image">
+            <div class="product-desc">
+            <p class="product-title">${this.product_name}</p>
+            <p class="product-quantity">Количество: ${this.quantity}</p>
+        <p class="product-single-price">${this.price} за ед.</p>
+        </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">${this.quantity*this.price} ₽</p>
+            <button class="del-btn" data-id="${this.id_product}">&times;</button>
+        </div>
+        </div>`;
+  }
+}
+
+const listContext = {
+  ProductList: ProductItem,
+  Cart: CartItem
+};
+
+let cart = new Cart();
+let products = new ProductList(cart);
+// const list = new ProductList();
